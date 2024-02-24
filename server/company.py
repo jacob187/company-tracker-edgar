eaa092b760194729739e00300b35ee49a8504a5b
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-02-23 20:38:56 -0500

    Refactor company.py to improve code structure and readability

--- server/company.py
+++ server/company.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Query
 from typing import Annotated
 from datetime import datetime
-from setup import initialize_companies_and_identity as ici
+from setup import Setup
 
 app = FastAPI()
 
@@ -10,33 +10,41 @@ app = FastAPI()
 async def get_company_info(
     tickers: Annotated[list[str], Query()], years_back: int | None = 1
 ):
-    """
-    Retrieves financial information for a list of companies.
 
-    Args:
-        tickers (list[str]): List of company tickers.
-        years_back (int, optional): Number of years to retrieve financial information for. Defaults to 2.
+    setup = Setup()
+    companies = await setup.initialize_companies(tickers)
+    company_facts_dataframe = get_company_facts(companies)
+    frames = generate_frames(years_back)
+    output = generate_output(tickers, company_facts_dataframe, frames)
 
-    Raises:
-        HTTPException: Raised when an invalid ticker is passed.
+    return output
 
-    Returns:
-        dict: Dictionary containing financial information for each company.
-    """
-   
 
-    companies = await ici(tickers)
+def get_company_facts(companies: dict) -> list:
     try:
         facts_of_companies = {company.get_facts() for company in companies.values()}
         company_facts_dataframe = [fact.to_pandas() for fact in facts_of_companies]
-
     except AttributeError:
         raise HTTPException(
             status_code=404, detail="One or more of the companies do not exist"
         )
+    return company_facts_dataframe
 
-    output = {}
 
+def generate_frames(years_back: int) -> list[str]:
+    current_year = datetime.now().year
+    frames = [
+        f"CY{year}Q{quarter}{suffix}"
+        for year in range(current_year - years_back, current_year + 1)
+        for quarter in range(1, 5)
+        for suffix in ["", "I"]
+    ] + [f"CY{year}" for year in range(current_year - years_back, current_year + 1)]
+    return frames
+
+
+def generate_output(
+    tickers: list[str], company_facts_dataframe: list, frames: list
+) -> dict:
     my_facts = [
         "EarningsPerShareBasic",
         "GrossProfit",
@@ -45,19 +53,6 @@ async def get_company_info(
         "Liabilities",
         "CommonStockValue",
     ]
-
-    # Get the current year
-    current_year = datetime.now().year
-
-    # Generate a list of frames for specified years back. Some with quarters, some without. This captures 10K and 10Q reports.
-    frames = [
-        f"CY{year}Q{quarter}{suffix}"
-        for year in range(current_year - years_back, current_year + 1)
-        for quarter in range(1, 5)
-        for suffix in ["", "I"]
-    ] + [f"CY{year}" for year in range(current_year - years_back, current_year + 1)]
-
-    # Create nested dictionary with company tickers and associated facts
     output = {
         ticker: {
             fact: dict(
@@ -70,5 +65,12 @@ async def get_company_info(
         }
         for ticker, company_df in zip(tickers, company_facts_dataframe)
     }
+    cleaned_output = remove_empty_facts(output)
+    return cleaned_output
 
-    return output
+
+def remove_empty_facts(data: dict) -> dict:
+    return {
+        ticker: {fact: values for fact, values in company.items() if values}
+        for ticker, company in data.items()
+    }
