0fc10d92ce162b2f58c1e6deb252a836baf7eae5
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-02-23 20:41:04 -0500

    Refactor setup.py and add Setup class

--- server/setup.py
+++ server/setup.py
@@ -2,24 +2,25 @@ from edgar import Company, set_identity
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-name = os.getenv("NAME")
-email = os.getenv("EMAIL")
+class Setup:
+    def __init__(self):
+        load_dotenv()
+        name = os.getenv("NAME")
+        email = os.getenv("EMAIL")
+        set_identity(f"{name} {email}")
 
+    async def initialize_companies(self, company_names: list) -> dict:
+        """
+        Initializes the companies and sets the identity.
 
-async def initialize_companies_and_identity(company_names: list):
-    """
-    Initializes the companies and sets the identity.
+        Args:
+            company_names (list): A list of company names.
 
-    Args:
-        company_names (list): A list of company names.
+        Returns:
+            dict: A dictionary containing the initialized companies.
+        """
 
-    Returns:
-        dict: A dictionary containing the initialized companies.
-    """
-    set_identity(f"{name} {email}")
+        companies = {name: Company(name) for name in company_names}
 
-    companies = {name: Company(name) for name in company_names}
-
-    return companies
+        return companies
